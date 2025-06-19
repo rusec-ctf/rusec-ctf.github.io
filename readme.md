804346715b7c2c968b27f9556afe36ab7306db5a
Purple-Chicken <purple.chickn@rutgers.edu>
2025-06-19 17:57:34 -0400

    Initial commit -PC

---
+++ readme.md
@@ -0,0 +1,26 @@
+# RUSEC CTF Landing Page
+    
+## Basic Website Structure
+    
+    Basic logo + name
+    links to (old) website, discord, ctftime, and theme changer
+
+    coming soon + date
+    call to join discord
+    physical address: CAVE
+    email: rusec4096
+    rizzcord
+    2 pics to test sticky header
+    copyright notice at bottom
+        3 easter eggs
+    
+
+## TODO: 
+    - [x] fix layout on sticky header
+    - [ ] fix sticky header pic
+    - [ ] fix hero area pic + flex
+    - [x] remove bottom two pics
+    - [ ] better font choice
+    - [x] better color choice for light + dark modes
+    - [ ] improve layout/visibility on various screen dimensions and zoom levels
+    - [x] Update info (new rusec website, delete discord link for now, ctftime event, address(??), email)
---
+++ script.js
@@ -0,0 +1,33 @@
+const themeToggle = document.getElementById('themeToggle');
+const body = document.body;
+
+// Check if user preference exists in local storage
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    body.classList.add(currentTheme);
+}
+function togglemenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "nav") {
+        x.className += " nav--open";
+    } else {
+        x.className = "nav";
+    }
+    var element = document.getElementById("menu-toggle");
+element.classList.toggle("menu-toggle--open");
+    console.log("Switching themes...");
+}
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+
+    // Store user preference in local storage
+    if (body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark-mode');
+        console.log("Girly mode Active");
+    } else {
+        localStorage.setItem('theme', '');
+        console.log("Dark Mode Active.");
+    }
+});
+// Easter egg function (not crucial to website)
+const girlie_pop_0x2742b7=girlie_pop_0x34c2;(function(_0x10990b,_0x2ea369){const _0x578ffd=girlie_pop_0x34c2,_0xa41deb=_0x10990b();while(!![]){try{const _0xa74b32=-parseInt(_0x578ffd(0x1c3))/0x1+-parseInt(_0x578ffd(0x1c4))/0x2+-parseInt(_0x578ffd(0x1c7))/0x3+-parseInt(_0x578ffd(0x1cb))/0x4*(-parseInt(_0x578ffd(0x1cc))/0x5)+-parseInt(_0x578ffd(0x1cd))/0x6+parseInt(_0x578ffd(0x1c6))/0x7*(parseInt(_0x578ffd(0x1c9))/0x8)+parseInt(_0x578ffd(0x1ca))/0x9;if(_0xa74b32===_0x2ea369)break;else _0xa41deb['push'](_0xa41deb['shift']());}catch(_0x2228aa){_0xa41deb['push'](_0xa41deb['shift']());}}}(girlie_pop_0x28a4,0x312d2));function girlie_pop_0x28a4(){const _0x1b8788=['mf9knv8','mtC4mJGZDLHKvfzr','otq1mdaZwffkuuHR','CNvJDgy','mtz0DuDZrLi','mta1mJm2nJrRDgniBxe','mZqWD0zTDuDt','mJm1nvnSyM9lBW','mtm3nZyZme9yD2PrCG','m1j9','nu4Wmfa','mtK0ode2v0ftr3PN','nJm4odqYuhvWy2Df'];girlie_pop_0x28a4=function(){return _0x1b8788;};return girlie_pop_0x28a4();}const girlie_pop_0x1a9e48=girlie_pop_0x2742b7(0x1c8)+'{H311'+girlie_pop_0x2742b7(0x1c5)+girlie_pop_0x2742b7(0x1cf)+girlie_pop_0x2742b7(0x1ce);function girlie_pop_0x34c2(_0x329274,_0x11c77d){const _0x28a4d3=girlie_pop_0x28a4();return girlie_pop_0x34c2=function(_0x34c25e,_0x6784e3){_0x34c25e=_0x34c25e-0x1c3;let _0x2b3b66=_0x28a4d3[_0x34c25e];if(girlie_pop_0x34c2['iNZhQf']===undefined){var _0x5a8ce2=function(_0x111a74){const _0x5a6474='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=';let _0x51ef22='',_0x200f13='';for(let _0x4629c8=0x0,_0x43b03f,_0x4f1428,_0x31cb65=0x0;_0x4f1428=_0x111a74['charAt'](_0x31cb65++);~_0x4f1428&&(_0x43b03f=_0x4629c8%0x4?_0x43b03f*0x40+_0x4f1428:_0x4f1428,_0x4629c8++%0x4)?_0x51ef22+=String['fromCharCode'](0xff&_0x43b03f>>(-0x2*_0x4629c8&0x6)):0x0){_0x4f1428=_0x5a6474['indexOf'](_0x4f1428);}for(let _0x1b8429=0x0,_0x25a318=_0x51ef22['length'];_0x1b8429<_0x25a318;_0x1b8429++){_0x200f13+='%'+('00'+_0x51ef22['charCodeAt'](_0x1b8429)['toString'](0x10))['slice'](-0x2);}return decodeURIComponent(_0x200f13);};girlie_pop_0x34c2['LqzKVX']=_0x5a8ce2,_0x329274=arguments,girlie_pop_0x34c2['iNZhQf']=!![];}const _0x1b4314=_0x28a4d3[0x0],_0x281666=_0x34c25e+_0x1b4314,_0x3cc2a5=_0x329274[_0x281666];return!_0x3cc2a5?(_0x2b3b66=girlie_pop_0x34c2['LqzKVX'](_0x2b3b66),_0x329274[_0x281666]=_0x2b3b66):_0x2b3b66=_0x3cc2a5,_0x2b3b66;},girlie_pop_0x34c2(_0x329274,_0x11c77d);}function girlie_pop_0x209d07(){console['log'](girlie_pop_0x1a9e48);}
